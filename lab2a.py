e97bf2cc93d946d60df558f6adfa0c66d217ad70
Husanpreet Kaur <husanpreet-kaur3@myseneca.ca>
2024-09-25 13:47:00 -0400

    lab2a.py added

---
+++ lab2a.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+  # This tells the terminal to use Python to run the script
+
+name = 'Jon'  # This stores the name Jon in the variable 'name'
+age = 20  # This stores the age 20 in the variable 'age'
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')  # This prints a message combining name and age
