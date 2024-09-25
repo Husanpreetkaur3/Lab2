c4228cb0906552e29b5fc10a0265766ad95165bb
Husanpreet Kaur <husanpreet-kaur3@myseneca.ca>
2024-09-25 13:47:22 -0400

    lab2a1.py added

---
+++ lab2a1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+colour = input("Type in a colour and press enter: ")  # Ask the user to input a colour
+print('The colour I typed in is: ' + colour)  # Print the input back to the user
