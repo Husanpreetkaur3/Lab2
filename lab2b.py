6ec22c752c94bff680615be00db54eebb6465c3d
Husanpreet Kaur <husanpreet-kaur3@myseneca.ca>
2024-09-25 13:47:40 -0400

    lab2b.py added

---
+++ lab2b.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+name = input("Name: ")  # Ask the user for their name
+age = input("Age: ")  # Ask the user for their age
+print(f"Hi {name}, you are {age} years old.")  # Use f-string to print the name and age
